52e51a1417b110708dd1ccf341633d95cbe7dc6e
takoyaki127 <s826062@gmail.com>
2023-05-01 23:08:51 +0900

    クラス変数にprivate修飾子を追加

--- src/Screen/MyPanel.java
+++ src/Screen/MyPanel.java
@@ -14,12 +14,12 @@ import Borad.Board;
 import Player.Player;
 import disk.Disks;
 
-public class MyPanel extends JPanel implements MouseListener{
-	Board board;
-	int cellHeight, cellWidth;
-	Player[] players;
-	int diskXSize, diskYSize; 
-	double diskSize = 0.7;
+public class MyPanel extends JPanel implements MouseListener {
+	private Board board;
+	private int cellHeight, cellWidth;
+	private Player[] players;
+	private int diskXSize, diskYSize;
+	private double diskSize = 0.7;
 
 	public MyPanel(String title, Board board, Player[] players) {
 		super(true);
@@ -30,109 +30,108 @@ public class MyPanel extends JPanel implements MouseListener{
 		frmContentPane.add(this);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new GridLayout());
-		frame.setBounds(1000,100,600, 600);
-		
+		frame.setBounds(1000, 100, 600, 600);
+
 		addMouseListener(this);
-		
+
 		frame.setVisible(true);
 	}
-	
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		drawHorizontalLine(g);
 		drawVerticalLine(g);
 		drawDisks(g);
 	}
-	
+
 	private void drawHorizontalLine(Graphics g) {
 		cellHeight = getHeight() / board.height();
-		for (int i=1;i<board.height();i++) {
+		for (int i = 1; i < board.height(); i++) {
 			int y = cellHeight * i;
 			g.drawLine(0, y, getWidth(), y);
 		}
 		diskYSize = (int) (cellHeight * diskSize);
 	}
-	
+
 	private void drawVerticalLine(Graphics g) {
 		cellWidth = getWidth() / board.width();
-		for (int i=1;i<board.width();i++) {
+		for (int i = 1; i < board.width(); i++) {
 			int x = cellWidth * i;
 			g.drawLine(x, 0, x, getHeight());
 		}
 		diskXSize = (int) (cellWidth * diskSize);
 	}
-	
+
 	int count = 0;
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		int[] pos = getCellAt(e.getPoint());
-//		System.out.println(pos[0]+ "" + pos[1]);
-		
+		// System.out.println(pos[0]+ "" + pos[1]);
+
 		boolean isValidMove = false;
-		if (count % players.length == 0 ) {
+		if (count % players.length == 0) {
 			isValidMove = players[0].put(pos[0], pos[1]);
+		} else {
+			isValidMove = players[1].put(pos[0], pos[1]);
 		}
-		else {
-			isValidMove = players[1].put(pos[0], pos[1]);			
-		}
-		if ( isValidMove) {
+		if (isValidMove) {
 			count++;
 			repaint();
 			finish();
 		}
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
 	}
-	
+
 	private int[] getCellAt(Point point) {
-		int xCellNum = (int)(point.getX() / cellWidth);
-		int yCellNum = (int)(point.getY() / cellHeight);
-		int[] pos = {xCellNum, yCellNum};
+		int xCellNum = (int) (point.getX() / cellWidth);
+		int yCellNum = (int) (point.getY() / cellHeight);
+		int[] pos = { xCellNum, yCellNum };
 		return pos;
 	}
-	
+
 	private void drawDisks(Graphics g) {
 		double margin = ((1 - diskSize) / 2);
-		for (int y=0;y<board.height();y++) {
-			for (int x=0;x<board.width();x++) {
-				int xdisk = (int)((x + margin) * cellWidth);
-				int ydisk = (int)((y + margin) * cellHeight);
+		for (int y = 0; y < board.height(); y++) {
+			for (int x = 0; x < board.width(); x++) {
+				int xdisk = (int) ((x + margin) * cellWidth);
+				int ydisk = (int) ((y + margin) * cellHeight);
 				drawDisk(g, xdisk, ydisk, x, y);
 			}
 		}
 	}
-	
-	private void drawDisk(Graphics g,int x,int y,int xNum, int yNum) {
+
+	private void drawDisk(Graphics g, int x, int y, int xNum, int yNum) {
 		if (board.get(xNum, yNum) == Disks.White) {
-			g.drawOval(x, y, diskXSize, diskYSize);			
-		}
-		else if (board.get(xNum, yNum) == Disks.Black) {
-			g.fillOval(x, y, diskXSize, diskYSize);			
+			g.drawOval(x, y, diskXSize, diskYSize);
+		} else if (board.get(xNum, yNum) == Disks.Black) {
+			g.fillOval(x, y, diskXSize, diskYSize);
 		}
 	}
-	
+
 	private void finish() {
 		if (board.isGameContinued() == false) {
 			board.result(players);
