7fdd4668df04d4251ae9a32fe648551a494c2c05
takoyaki127 <s826062@gmail.com>
2023-05-01 09:47:26 +0900

    不要なコメントを削除

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -3,39 +3,37 @@ package Player;
 import Borad.Board;
 import disk.Disks;
 
-public class Player implements Put{
+public class Player implements Put {
 	private Board board;
 	private String name;
-//	private Disk myDisk, enemyDisk;
 	private Disks myDisk, enemyDisk;
-	
+
 	public Player(Board board, String name, Disks myDisk, Disks enemyDisk) {
 		this.board = board;
 		this.myDisk = myDisk;
 		this.enemyDisk = enemyDisk;
 		this.name = name;
 	}
-	
+
 	public String name() {
 		return name;
 	}
-	
+
 	public Disks mycolor() {
-//		return myDisk.color();
 		return myDisk;
 	}
-	
+
 	public Disks enemyColor() {
 		return enemyDisk;
 	}
-	
+
 	public boolean put(int x, int y) {
 		if (board.put(this, x, y)) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	public void win() {
 		System.out.println(name + " Win!!");
 	}
