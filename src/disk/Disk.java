7ad4dc0ece275bdd7a74c2d68dc292c0ff653833
takoyaki127 <s826062@gmail.com>
2023-05-04 23:19:27 +0900

    setterをprivateに変更

--- src/disk/Disk.java
+++ src/disk/Disk.java
@@ -1,29 +1,33 @@
 package disk;
 
+import Screen.Cell;
+
 public class Disk {
 	private double diskSize;
-	private int diskXSize,diskYSize;
+	private int diskXSize, diskYSize;
 
-	public Disk(double diskSize) {
+	public Disk(double diskSize, Cell cell) {
 		this.diskSize = diskSize;
+		setY(cell);
+		setX(cell);
 	}
-	
+
 	public double size() {
 		return diskSize;
 	}
-	
-	public void setY(int cellHeight) {
-		diskYSize = (int) (cellHeight * diskSize);
+
+	private void setY(Cell cell) {
+		diskYSize = (int) (cell.height() * diskSize);
 	}
-	
+
 	public int y() {
 		return diskYSize;
 	}
-	
-	public void setX(int cellWidth) {
-		diskXSize = (int) (cellWidth * diskSize);
+
+	private void setX(Cell cell) {
+		diskXSize = (int) (cell.width() * diskSize);
 	}
-	
+
 	public int x() {
 		return diskXSize;
 	}
