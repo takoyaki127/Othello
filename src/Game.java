bfcb4a98aa09ad8ba25bb729fd4630d166ed825e
takoyaki127 <s826062@gmail.com>
2023-05-04 22:46:07 +0900

    不要なコメントを削除

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 import Borad.Board;
 import Player.Player;
 import Player.Player1;
@@ -7,64 +5,17 @@ import Player.Player2;
 import Screen.MyPanel;
 
 public class Game {
-//	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		Board board = new Board(8, 8);
-//		board.display();
-		
 
 		// プレイヤーを生成
 		Player player1 = new Player1(board);
 		Player player2 = new Player2(board);
 		Player[] players = { player1, player2 };
-		
-		new MyPanel("Othello",board, players);
 
-		// ゲーム中
-//		while (board.isGameContinued()) {
-//			inputDiskPosition(player1, board);
-//			inputDiskPosition(player2, board);
-//		}
+		new MyPanel("Othello", board, players);
 
-		// 結果
-//		board.result(players);
-
-//		sc.close();
 	}
 
-//	private static boolean passCheck(int x, int y) {
-//		if (x < 0 || y < 0) {
-//			return true;
-//		}
-//		return false;
-//	}
-
-//	private static int[] scan(Player player) {
-//		int[] pos = new int[2];
-//		while (true) {
-//			System.out.println("負の値を入力でパス");
-//			System.out.print(player.name() + "->");
-//			try {
-//				pos[0] = Integer.parseInt(sc.next());
-//				pos[1] = Integer.parseInt(sc.next());
-//				break;
-//			} catch (Exception e) {
-//				System.out.println("不正な値です\n");
-//			}
-//		}
-//		return pos;
-//	}
-
-//	private static void inputDiskPosition(Player player, Board board) {
-//		boolean putDisk = false;
-//		while (board.isGameContinued() && putDisk == false) {
-//			int[] pos = scan(player);
-//			if (passCheck(pos[0], pos[1])) {
-//				break;
-//			}
-//			putDisk = player.put(pos[0], pos[1]);
-//		}
-//		board.display();
-//	}
 }
