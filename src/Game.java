5e6281dabc2604423bf1bfdb63c1d9dc44fda104
takoyaki127 <s826062@gmail.com>
2023-05-06 17:06:38 +0900

    不要なスペースを削除

--- src/Game.java
+++ src/Game.java
@@ -9,7 +9,7 @@ public class Game {
 
 		// プレイヤーを生成
 		Player player1 = Player.getInstance("Player1", board);
-		Player player2 = Player.getInstance("Player2", board);		
+		Player player2 = Player.getInstance("Player2", board);
 		Player[] players = { player1, player2 };
 
 		new MyPanel("Othello", board, players);
