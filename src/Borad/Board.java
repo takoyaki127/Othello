919642acbdaeab76388deff1488a8d8d2e5d85ba
takoyaki127 <s826062@gmail.com>
2023-05-06 20:04:42 +0900

    isGameContinuedメソッドを修正

--- src/Borad/Board.java
+++ src/Borad/Board.java
@@ -11,66 +11,74 @@ public class Board {
 		setup();
 	}
 
-	public boolean put(Player player, int x, int y) {
-		if (update(x, y, player)) {
-			board[y][x] = player.mycolor();
+	private void setup() {
+		int row_center = (height() - 1) / 2;
+		int col_center = (width() - 1) / 2;
+
+		putDisk(DiskColor.White, row_center, col_center);
+		putDisk(DiskColor.White, row_center + 1, col_center + 1);
+
+		putDisk(DiskColor.Black, row_center + 1, col_center);
+		putDisk(DiskColor.Black, row_center, col_center + 1);
+	}
+
+	public int height() {
+		return this.board.length;
+	}
+
+	public int width() {
+		return this.board[0].length;
+	}
+	
+	public boolean playerPut(Player player, int x, int y) {
+		if (roundUpdate(x, y, player)) {
+			putDisk(player.mycolor(), x, y);
 			return true;
 		}
 		return false;
 	}
-
-	private void put(DiskColor disk, int x, int y) {
-		board[y][x] = disk;
+	
+	private void putDisk(DiskColor disk, int x, int y) {
+		try {
+			board[y][x] = disk;
+		}
+		catch(IndexOutOfBoundsException e) {
+			e.printStackTrace();
+		}
 	}
-
+	
 	public DiskColor get(int x, int y) {
 		try {
 			return board[y][x];
-		} catch (Exception e) {
+		} catch (IndexOutOfBoundsException e) {
 			return null;
 		}
 	}
 
-	private void setup() {
-		int row_center = board.length / 2 - 1;
-		int col_center = board[0].length / 2 - 1;
-
-		put(DiskColor.White, row_center, col_center);
-		put(DiskColor.White, row_center + 1, col_center + 1);
-
-		put(DiskColor.Black, row_center + 1, col_center);
-		put(DiskColor.Black, row_center, col_center + 1);
-	}
-
-	private boolean update(int x, int y, Player player) {
-		boolean isValidMove = false;
+	private boolean roundUpdate(int x, int y, Player player) {
 		if (get(x, y) != null) {
 			return false;
 		}
+		
+		boolean isValidMove = false;
 		for (int dy = -1; dy < 2; dy++) {
 			for (int dx = -1; dx < 2; dx++) {
-				if (dx == 0 && dy == 0) {
-					continue;
-				}
-				if (validateMove(x, y, dx, dy, player)) {
+				if (directUpdate(x, y, dx, dy, player)) {
 					isValidMove = true;
 				}
-
 			}
 		}
 		return isValidMove;
 	}
 
-	private boolean validateMove(int x, int y, int dx, int dy, Player player) {
-		int x_next = x + dx;
-		int y_next = y + dy;
-		if (get(x_next, y_next) == player.enemyColor()) {
-			if (get(x_next + dx, y_next + dy) == player.mycolor()) {
-				board[y_next][x_next] = player.mycolor();
+	private boolean directUpdate(int x, int y, int dx, int dy, Player player) {
+		if (get(x+dx, y+dy) == player.enemyColor()) {
+			if (get(x+dx + dx, y+dy + dy) == player.mycolor()) {
+				putDisk(player.mycolor(), x + dx, y + dy);
 				return true;
 			}
-			if (validateMove(x_next, y_next, dx, dy, player)) {
-				board[y_next][x_next] = player.mycolor();
+			if (directUpdate(x + dx, y + dy, dx, dy, player)) {
+				putDisk(player.mycolor(), x + dx, y + dy);
 				return true;
 			}
 		}
@@ -78,27 +86,19 @@ public class Board {
 	}
 
 	public boolean isGameContinued(Player[] players) {
-		int count = 0;
-		for (int i = 0; i < board.length; i++) {
-			for (int k = 0; k < board[0].length; k++) {
-				if (board[i][k] == null) {
-					count++;
-				}
+		for (Player player : players) {
+			if(playerCanPut(player)) {
+				return true;
 			}
 		}
-		if (count == 0) {
-			return false;
-		}
-		return allPlayerCanPut(players);
+		return false;
 	}
 
-	private boolean allPlayerCanPut(Player[] players) {
-		for (Player player : players) {
-			for (int y = 0; y < board.length; y++) {
-				for (int x = 0; x < board[y].length; x++) {
-					if (canPut(x, y, player)) {
-						return true;
-					}
+	private boolean playerCanPut(Player player) {
+		for (int y = 0; y < board.length; y++) {
+			for (int x = 0; x < board[y].length; x++) {
+				if (canPut(x, y, player)) {
+					return true;
 				}
 			}
 		}
@@ -137,9 +137,9 @@ public class Board {
 
 	private int diskCount(Player player) {
 		int count = 0;
-		for (int i = 0; i < board.length; i++) {
-			for (int k = 0; k < board[i].length; k++) {
-				if (board[i][k] == player.mycolor()) {
+		for (int y = 0; y < board.length; y++) {
+			for (int x = 0; x < board[y].length; x++) {
+				if (get(x, y) == player.mycolor()) {
 					count++;
 				}
 			}
@@ -164,18 +164,8 @@ public class Board {
 			}
 		}
 		if (drawCount >= 2) {
-			System.out.println("draw");
 			return null;
 		}
-		winner.win();
 		return winner;
-	}
-
-	public int height() {
-		return this.board.length;
-	}
-
-	public int width() {
-		return this.board[0].length;
 	}
 }
